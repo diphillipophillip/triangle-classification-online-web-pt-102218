6cc0e5d9988750bafca150718f180d8ff0282e2b
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-12-07 01:30:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,19 @@
+require 'pry'
 class Triangle
-  # write code here
+  
+  attr_accessor :num, :num1, :num2
+  @@numbers = []
+  
+  def initialize(num, num1, num2)
+    @num = num 
+    @num1 = num1 
+    @num2 = num2 
+    @@numbers << self
+  end 
+  
+  def kind 
+    @@numbers
+    
+  end 
 end
+
