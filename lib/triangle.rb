9b109a0534c89646fb6a89d17794afcbda69ad3a
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-12-07 00:35:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,11 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :num, :num1, :num2
+  
+  def initialize(num, num1, num2)
+    @num = num 
+    @num1 = num1 
+    @num2 = num2 
+  end 
 end
+
