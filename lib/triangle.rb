d94a376c875b0a795442b822d684373a9aaf6c75
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-12-07 01:53:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,22 @@
 class Triangle
-  # write code here
-end
+  
+  attr_accessor :num, :num1, :num2
+  
+  
+  def initialize(num, num1, num2)
+    @num = num 
+    @num1 = num1 
+    @num2 = num2 
+  end 
+
+  def kind 
+    if num == num1 && num == num2 
+      :equilateral
+    elsif num == num1 || num == num2 || num1 == num2 
+      :isosceles 
+    else :scalene
+      
+    end 
+    
+  end 
+end 
